491630b039c5f27fcdf0d9fcc6269d89be2e12fd
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-03-25 17:47:01 -0300

    parte de Jugador y Defensor

--- scr/Defensor.cs
+++ scr/Defensor.cs
@@ -1,6 +1,9 @@
 namespace scr;
 
-public class Defensor
+public class Defensor : Jugador
 {
     public double Quite { get; set; }
+
+    public override double GetPrecision() => Quite;
+    public override double GetVisionGeneral() => VisionJuego + VisionCompaneros;
 }
--- scr/Jugador.cs
+++ scr/Jugador.cs
@@ -2,8 +2,29 @@ namespace scr;
 
 public abstract class Jugador
 {
-    public double visionJuego { get; set; }
-    public double visionCompaneros { get; set; }
+    public double VisionJuego { get; set; }
+    public double VisionCompaneros { get; set; }
     public double Potencia { get; set; }
-    public double habilidadPases { get; set; }
+    public double HabilidadPases { get; set; }
+
+    public abstract double GetPrecision();
+    public abstract double GetVisionGeneral();
+    
+    public void AplicarEntrenamientoFisico()
+    {
+        Potencia += 1;
+        HabilidadPases += 0.5;
+    }
+    
+    public void AplicarEntrenamientoLirico()
+    {
+        HabilidadPases += 1;
+        VisionJuego += 0.5;
+    }
+    
+    public void AplicarEntrenamientoTactico()
+    {
+        VisionJuego += 0.5;
+        VisionCompaneros += 0.5;
+    }
 }
